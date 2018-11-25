927d879e04a37b3c462e881e616040b8222275b8
Zoran Donev <zorandonev@gmail.com>
2018-11-25 09:56:07 -0800

    Add Test Shell to the Solution

---
+++ test/unit/UnitTests/AgreementServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace EnterpriseMessagingGateway.UnitTests
+{
+    [TestClass]
+    public class AgreementServiceTests
+    {
+        [TestMethod]
+        public void Should_ReturnAgreement_When_CallingService()
+        {
+            //Arrage
+            var expectedValue = "ExpectedValue";
+
+
+            //Act
+            var currentValue = "ExpectedValue";
+
+            //Assert
+            currentValue.Should().BeEquivalentTo(expectedValue);
+        }
+    }
+}
---
+++ test/unit/UnitTests/Initialize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnterpriseMessagingGateway.UnitTests
+{
+    [TestClass]
+    public class Initialize
+    {
+        [AssemblyInitialize]
+        public static void TestInitializer(TestContext context)
+        {
+
+        }
+
+        [AssemblyCleanup]
+        public static void TestCleanup()
+        {
+
+        }
+
+    }
+}
